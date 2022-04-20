6ca5e009c435092ed5b412f93c49970b34399ea3
G10on <jitenparwani1c17@gmail.com>
2022-04-20 01:17:09 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import headerData from '/src/assets/json/templates/headerdata.json';
+import headerData from '../assets/json/templates/headerdata.json';
 
 @Component({
   selector: 'app-root',

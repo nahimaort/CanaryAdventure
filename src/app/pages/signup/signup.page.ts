be91220302a9923229f359acd4ba3932a39ecd9b
Nahima Ortega <56322714+nahimaort@users.noreply.github.com>
2022-05-19 11:26:53 +0100

    Sprint 4 v2.2 signup page with email and password

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -25,7 +25,11 @@ export class SignupPage implements OnInit {
       lastName: new FormControl(this.newUserData.lastName, [Validators.required, Validators.maxLength(50), Validators.pattern('^[A-Za-z]+$')]),
       country: new FormControl(this.newUserData.country, Validators.required),
       // eslint-disable-next-line max-len
-      phone: new FormControl(this.newUserData.phone, [Validators.required, Validators.pattern('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')])
+      phone: new FormControl(this.newUserData.phone, [Validators.required, Validators.pattern('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')]),
+      // eslint-disable-next-line max-len
+      email: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$')]),
+      // eslint-disable-next-line max-len
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
   get firstName() {
@@ -44,10 +48,19 @@ export class SignupPage implements OnInit {
     return this.userDataForm.get('phone');
   }
 
+  get email() {
+    return this.userDataForm.get('email');
+  }
+
+  get password() {
+    return this.userDataForm.get('password');
+  }
+
   ngOnInit() {
   }
 
   saveUserData() {
+    /*
     const navigationExtras: NavigationExtras = {
       queryParams: {
         user: this.newUserData
@@ -55,6 +68,9 @@ export class SignupPage implements OnInit {
     };
     console.log(navigationExtras.queryParams.user);
     this.navCtrl.navigateForward('/signup-account', navigationExtras);
+    */
     console.log(this.newUserData);
+    console.log(this.email.value);
+    console.log(this.password.value);
   }
 }

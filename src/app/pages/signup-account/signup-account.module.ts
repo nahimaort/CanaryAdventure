ce98790c6aca11aa95c60440097c8adc10685eba
Nahima Ortega <56322714+nahimaort@users.noreply.github.com>
2022-05-19 11:15:16 +0100

    Sprint 4 v2.2 signup second slide

--- src/app/pages/signup-account/signup-account.module.ts
+++ src/app/pages/signup-account/signup-account.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignupAccountPageRoutingModule } from './signup-account-routing.module';
 
 import { SignupAccountPage } from './signup-account.page';
+import {ComponentsModule} from "../../components/components.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignupAccountPageRoutingModule
+    SignupAccountPageRoutingModule,
+    ReactiveFormsModule,
+    ComponentsModule
   ],
   declarations: [SignupAccountPage]
 })
--- src/app/pages/signup-account/signup-account.page.ts
+++ src/app/pages/signup-account/signup-account.page.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserData} from '../../model/interfaces.model';
 import {ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-account',
@@ -9,8 +10,24 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class SignupAccountPage implements OnInit {
   user: any;
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  userAccountForm: FormGroup;
 
   constructor(private route: ActivatedRoute) {
+    this.userAccountForm = new FormGroup({
+      // eslint-disable-next-line max-len
+      email: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$')]),
+      // eslint-disable-next-line max-len
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  get email() {
+    return this.userAccountForm.get('email');
+  }
+
+  get password() {
+    return this.userAccountForm.get('password');
   }
 
   ngOnInit() {
@@ -19,7 +36,7 @@ export class SignupAccountPage implements OnInit {
     });
   }
 
-  storeData() {
-    console.log(this.user);
+  saveUserData() {
+
   }
 }
